aedcef5596843c9d32b39f8c3ce592ad2854be91
KrisKaelep <kris.kaelep@gmail.com>
2017-04-18 18:21:40 +0300

    Update Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,7 @@
 public class Main {
       public static void main [ ] ) {
-            System.out.println (*Alustame pommitamisega! * ) ;
-      }
-}
+            System.out.println ("Alustame pommitamisega")
+            System.out.println (" Aasta 2017")
+            System.out.println (" Kris")
+            
+            
